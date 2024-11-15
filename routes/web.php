1cdbadb511b6f913bbfa599866e120fdf51e8153
Ace <akhmadfauzy40@gmail.com>
2024-11-15 10:55:17 +0700

    done: setup template & middleware role

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'role:admin'], function () {
+
+});
+
+Route::group(['middleware' => 'role:user'], function () {
+
+});
